32093cb1aa088ba9c8454eb3b1915e71bdf2711a
cathfan03 <cathyfan@umich.edu>
2026-09-20 23:00:48 -0400

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> organizer;
+
+        for(int num: nums){
+            organizer[num]++;
+        }
+        
+        
+        vector<pair<int, int>> arr;
+        for (const auto&p : organizer){
+            arr.push_back({p.second, p.first});
+        }
+        sort (arr.rbegin(), arr.rend());
+
+        vector<int> res;
+        for (int i = 0; i < k; i++){
+            res.push_back(arr[i].second);
+        }
+        return res;
+    }
+};
